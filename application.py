4e88593bab4493bd19d99b59b79a31ec19e24c0e
venkat2k <venkatkumarcs@gmail.com>
2019-07-16 18:29:15 +0530

    added import.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import *
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -23,4 +23,26 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/login", methods = ["POST"])
+def login():
+	username = request.form.get("username")
+	password = request.form.get("password")
+	msg = "failure"
+	count = db.execute("SELECT count(*) FROM userdata where username = :username and password = :password", {"username": username, "password": password}).fetchone()
+	if count[0] == 1:
+		msg = "success"
+	else: msg = str(count)
+	db.commit()
+	return render_template("success.html", result = msg)
+
+@app.route("/signup", methods = ["POST"])
+def signup():
+	username = request.form.get("username")
+	password = request.form.get("password")
+	display_name = request.form.get("display_name")
+	db.execute("INSERT INTO userdata (username, password, displayname) VALUES (:username, :password, :displayname)",
+		{"username": username, "password": password, "displayname": display_name})
+	db.commit()
+	return render_template("success.html", result = "success")
